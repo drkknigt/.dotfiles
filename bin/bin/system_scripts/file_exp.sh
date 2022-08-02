7c5638dca2a4dcf73ab4c8b64afd51e782c67cc6
drkknigt <vitobnd79@gmail.com>
2022-08-02 22:48:47 +0530

    changed config

--- bin/bin/system_scripts/file_exp.sh
+++ bin/bin/system_scripts/file_exp.sh
@@ -4,22 +4,18 @@ exp=$1
 # # file="$(fzf --height 40% --reverse)" && [ -f "$file" ] && xdg-open "$file" 
 # fdfind --type f . | fzf  --cycle | xdg-open 2> ~/error.txt
 # # compgen -c | fzf | zsh
-if [ -d pat ] 
+if [ -d disk ] 
 then
-    pat=pat
-    down=down 
-    doc=doc
+    disk=disk
 else
-    pat=''
-    down=''
-    doc=''
+    disk=''
 fi
 
 if [ ${exp} = "f" ]
 then
-    file=$(fdfind --type ${exp} . $down $pat $doc $HOME | fzf --cycle --prompt='open files: ' --delimiter / --with-nth -1 --bind "tab:execute(setsid -f xdg-open {})")
+    file=$(fdfind --type ${exp} . $disk $HOME | fzf --cycle --prompt='open files: ' --delimiter / --with-nth -1 --bind "tab:execute(setsid -f xdg-open {})")
 else
-    file=$(fdfind --type ${exp} . ${pat} ${down} ${doc} $HOME | fzf --cycle --prompt='open directories: ' )
+    file=$(fdfind --type ${exp} . ${disk} $HOME | fzf --cycle --prompt='open directories: ' )
 fi
 echo $file
 
--- bin/bin/system_scripts/mount.sh
+++ bin/bin/system_scripts/mount.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 source /home/drkknght/.zshrc
+mkdir ~/disk 2> /dev/null
 
 selected_device=$(lsblk -l | fzf | cut -d " " -f 1)
 
 if [ -z $selected_device ]; then
     exit
 elif grep -qs "/dev/$selected_device"  /proc/mounts; then
-    sudo umount "/dev/$selected_device" /media/drkknght/disk/ 
+    sudo umount "/dev/$selected_device" ~/disk
 else
-    sudo mount "/dev/$selected_device" /media/drkknght/disk/ 
+    sudo mount "/dev/$selected_device" ~/disk
 fi
