28b40a14e117f391eca42750bd07764436f19f98
drkknigt <vitobnd79@gmail.com>
2023-01-20 22:03:33 +0530

    changed config

--- nvim/.config/nvim/lua/lazy-nvim/init.lua
+++ nvim/.config/nvim/lua/lazy-nvim/init.lua
@@ -359,7 +359,42 @@ require("lazy").setup({
 		event = "CursorHold",
 		config = function()
 			-- require("userPlugins.lspsaga-config")
-			require("lspsaga").setup({})
+			require("lspsaga").setup({
+
+				ui = {
+					-- currently only round theme
+					theme = "round",
+					-- this option only work in neovim 0.9
+					title = true,
+					-- border type can be single,double,rounded,solid,shadow.
+					border = "double",
+					winblend = 0,
+					expand = "",
+					collapse = "",
+					preview = " ",
+					code_action = "💡",
+					diagnostic = "🐞",
+					incoming = " ",
+					outgoing = " ",
+					colors = {
+						--float window normal background color
+						normal_bg = "None",
+						--title background color
+						title_bg = "None",
+						red = "#e95678",
+						magenta = "#b33076",
+						orange = "#FF8700",
+						yellow = "#f7bb3b",
+						green = "#afd700",
+						cyan = "#36d0e0",
+						blue = "#61afef",
+						purple = "#CBA6F7",
+						white = "#d1d4cf",
+						black = "white",
+					},
+					kind = {},
+				},
+			})
 		end,
 	},
 
