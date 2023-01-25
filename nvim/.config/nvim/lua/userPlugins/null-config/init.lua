0e73f72ad0ab65c4cdcec6fa5627592c6117f789
drkknigt <vitobnd79@gmail.com>
2023-01-25 14:43:57 +0530

    changed config

--- nvim/.config/nvim/lua/userPlugins/null-config/init.lua
+++ nvim/.config/nvim/lua/userPlugins/null-config/init.lua
@@ -11,7 +11,7 @@ sources = {
 	null_ls.builtins.formatting.prettierd,
 	null_ls.builtins.diagnostics.luacheck,
 	null_ls.builtins.formatting.stylua,
-	null_ls.builtins.code_actions.gitsigns,
+	-- null_ls.builtins.code_actions.gitsigns,
 	-- null_ls.builtins.formatting.clang_format,
 	-- null_ls.builtins.diagnostics.pylint.with({
 	--     filetypes = {"python"},
