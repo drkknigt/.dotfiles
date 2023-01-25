e03bc7e02e1f02379762cba76ff306c485a06d48
drkknigt <vitobnd79@gmail.com>
2023-01-25 14:43:00 +0530

    changed config

--- nvim/.config/nvim/lua/userPlugins/null-config/init.lua
+++ nvim/.config/nvim/lua/userPlugins/null-config/init.lua
@@ -1,24 +1,23 @@
-local null_ls = require( "null-ls" )
+local null_ls = require("null-ls")
 sources = {
-    null_ls.builtins.formatting.black.with(
-        { extra_args = { "--fast", "--skip-string-normalization" } }
-     ),
-    null_ls.builtins.diagnostics.flake8,
-    -- null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.formatting.isort,
-    require( "userPlugins.null-config.builtins.formatting.jsbeautify" ),
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.code_actions.shellcheck,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.luacheck,
-    null_ls.builtins.formatting.stylua,
-    -- null_ls.builtins.formatting.clang_format,
-    -- null_ls.builtins.diagnostics.pylint.with({
-    --     filetypes = {"python"},
-    --     command = "pylint",
-    --     args = {"--from-stdin","$FILENAME","-f","json"},
-    -- }),
+	null_ls.builtins.formatting.black.with({ extra_args = { "--fast", "--skip-string-normalization" } }),
+	null_ls.builtins.diagnostics.flake8,
+	-- null_ls.builtins.diagnostics.mypy,
+	null_ls.builtins.formatting.isort,
+	require("userPlugins.null-config.builtins.formatting.jsbeautify"),
+	null_ls.builtins.diagnostics.eslint_d,
+	null_ls.builtins.diagnostics.shellcheck,
+	null_ls.builtins.code_actions.shellcheck,
+	null_ls.builtins.formatting.prettierd,
+	null_ls.builtins.diagnostics.luacheck,
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.code_actions.gitsigns,
+	-- null_ls.builtins.formatting.clang_format,
+	-- null_ls.builtins.diagnostics.pylint.with({
+	--     filetypes = {"python"},
+	--     command = "pylint",
+	--     args = {"--from-stdin","$FILENAME","-f","json"},
+	-- }),
 }
 
 -- require("lspconfig")["null-ls"].setup({
@@ -27,14 +26,11 @@ sources = {
 -- 	end,
 -- })
 
-null_ls.setup(
-    {
-        on_attach = function()
-            vim.cmd( [[ autocmd BufWritePre <buffer> lua vim.lsp.buf.format() ]] )
-        end
-,
-        sources = sources,
-        debug = false,
-        debounce = 150,
-    }
- )
+null_ls.setup({
+	on_attach = function()
+		vim.cmd([[ autocmd BufWritePre <buffer> lua vim.lsp.buf.format() ]])
+	end,
+	sources = sources,
+	debug = false,
+	debounce = 150,
+})
