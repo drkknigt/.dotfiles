82ad197265ec9ddc0d7394d2a87f25d053a17848
drkknigt <vitobnd79@gmail.com>
2023-06-03 10:08:25 +0530

    added configs for xinit and bin

---
+++ bin/bin/system_scripts/scale.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $1 = '0' ]
+then
+    monitor=$(xrandr | grep -Pi '\bconnected\b' | sed -n 1,1p | cut -d " " -f1)
+elif [ $1 = '1' ]
+then
+    $monitor=$(xrandr | grep -Pi '\bconnected\b' | sed -n 2,2p | cut -d " " -f1)
+fi
+
+zoom_options=$(seq -100 10 100 |  awk '{print $0"% zoom"}' | fzf --prompt="scale display in percent: " | grep -oP '[+-]*\d+')
+
+if [[ -n $zoom_options ]]; then
+    
+  zoom_percentage=$(( -1 * "zoom_options" ))
+  scale_factor=$(echo "scale=1; (100+$zoom_percentage) / 100" | bc)
+  xrandr --output eDP --scale "$scale_factorx$scale_factor"
+  
+fi
+
