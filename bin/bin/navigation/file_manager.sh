e393c6f28fc472acbaa3c1f59f5315b6c49f0807
drkknght <vitobnd79@gmail.com>
2023-08-22 00:51:30 +0530

    changed lfrc

--- bin/bin/navigation/file_manager.sh
+++ bin/bin/navigation/file_manager.sh
@@ -8,7 +8,7 @@ export FZF_DEFAULT_OPTS='--layout=reverse --border=sharp'
 export PATH=/home/drkknght/go/bin/:$PATH
 export PATH=/home/drkknght/usr/bin/:$PATH
 
-if [  "$current_dir_flag" = "0" ] ; then
+if [  "$current_dir_flag" = "0" ] || [ "$current_dir_flag" = "2" ] ; then
 direc=$(find ~ -type d |  fzf --cycle --prompt='change directory: ' --preview="tree -L 1 {} | batcat --theme='Monokai Extended Origin' --color=always" )
 if [  -z "$direc" ] ; then
             exit
@@ -16,7 +16,7 @@ fi
 fi
 
 if [ "$current_dir_flag" = "2" ] ; then
-    lfrun ~
+    lfrun "$direc"
     exit
 fi
 
