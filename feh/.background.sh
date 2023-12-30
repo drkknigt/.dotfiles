97ee94490bdad7dafac6bfc90a3baab4f78297d1
drkknght <vitobnd79@gmail.com>
2023-12-30 12:24:47 +0530

    v1.0

--- feh/.background.sh
+++ feh/.background.sh
@@ -3,10 +3,10 @@ current_wm=$(wmctrl -m | grep -Po "wlroots|i3")
 current_picture=$(cat ~/.cache/wallpaper.bg)
 if [ "$current_wm" = "i3" ]; then
     feh --no-fehbg --bg-fill ${current_picture} ${current_picture}
-    echo "hi"
 elif [ "$current_wm" = "wlroots" ]; then
     swaymsg output eDP-1 bg "$current_picture" fill
-    echo "hello"
+    # killall swaybg
+    # swaybg -i "$current_picture" -m fill
 fi
 
     
