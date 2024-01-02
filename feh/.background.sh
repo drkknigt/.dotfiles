da12876959a52202ccd89d4566aa5b89f95bac6b
drkknght <vitobnd79@gmail.com>
2024-01-02 23:43:19 +0530

    changed background.sh

--- feh/.background.sh
+++ feh/.background.sh
@@ -4,7 +4,8 @@ current_picture=$(cat ~/.cache/wallpaper.bg)
 if [ "$current_wm" = "i3" ]; then
     feh --no-fehbg --bg-fill ${current_picture} ${current_picture}
 elif [ "$current_wm" = "wlroots" ]; then
-    swaymsg output eDP-1 bg "$current_picture" fill
+    monitor=$(swaymsg -t get_outputs | jq -r '.[].name' | sed -n 1,1p)
+    swaymsg output "$monitor" bg "$current_picture" fill
     # killall swaybg
     # swaybg -i "$current_picture" -m fill
 fi
