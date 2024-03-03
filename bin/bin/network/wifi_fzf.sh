cdf17b09c1ca007da702f38bd4d19ddc1562d058
drkknght <vitobnd79@gmail.com>
2024-03-03 20:59:39 +0530

    changed wifi script for connecting to AP's v1.0-c1

--- bin/bin/network/wifi_fzf.sh
+++ bin/bin/network/wifi_fzf.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env zsh
-# source ~/.zshrc
+
 export FZF_DEFAULT_COMMAND='fdfind . --absolute-path --hidden'
 export FZF_DEFAULT_OPTS='--layout=reverse --border=sharp'
 export PATH=$HOME/.local/bin/:$PATH
-#
+
 selected_wifi=$(nmcli dev wifi list| fzf --prompt='Connect wifi: ')
 if [ -z "$selected_wifi" ] 
 then exit
 fi
 
-active_flag=$(echo $selected_wifi | xargs | awk -F" " '{ print $4 }')
-echo $active_flag
-if [ ${active_flag} = "*" ] 
-then
-    echo "exit" > /dev/null
-else
-    nmcli dev wifi connect $(echo $selected_wifi | xargs | cut -d ' ' -f2) > /dev/null
+if [[ "$(echo $selected_wifi | awk '{ print $1 }')" = "*" ]]; then
+    echo "already connected"
+    exit
 fi
+
+current_wifi=$(echo $selected_wifi | xargs | awk -F"Infra" '{ print $1 }' | awk '{ sub(/^[^ ]+/,""); print }' | sed 's/^ *//;s/ *$//')
+
+nmcli dev wifi connect $current_wifi > /dev/null
