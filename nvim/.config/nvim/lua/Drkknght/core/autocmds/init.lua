8c247a033b281f1c6fb2cc9ad8283edbe72224af
drkknght <vitobnd79@gmail.com>
2024-07-26 09:28:27 +0530

    refactored highlight on yank function

--- nvim/.config/nvim/lua/Drkknght/core/autocmds/init.lua
+++ nvim/.config/nvim/lua/Drkknght/core/autocmds/init.lua
@@ -1,10 +1,12 @@
 local api = vim.api
 
 -- Highlight on yank
-local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
 api.nvim_create_autocmd("TextYankPost", {
-	command = "silent! lua vim.highlight.on_yank({timeout = 440})",
-	group = yankGrp,
+	desc = "highlight when yanking text",
+	group = api.nvim_create_augroup("YankHighlight", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ timeout = 440 })
+	end,
 })
 
 -- set folds
