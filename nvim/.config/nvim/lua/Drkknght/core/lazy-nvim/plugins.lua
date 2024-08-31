5d3c9498624ab034886e5c934306e8aef67ecb4d
drkknght <vitobnd79@gmail.com>
2024-08-31 20:58:43 +0530

    configured vim-matchup to be compatible with nvim-cmp

--- nvim/.config/nvim/lua/Drkknght/core/lazy-nvim/plugins.lua
+++ nvim/.config/nvim/lua/Drkknght/core/lazy-nvim/plugins.lua
@@ -221,8 +221,17 @@ local matchup = {
 	-- https://github.com/andymass/vim-matchup
 	"andymass/vim-matchup",
 	event = "VeryLazy",
-	config = function()
-		vim.g.matchup_matchparen_offscreen = { method = "popup" }
+	config = function(_, opts)
+		local ok, cmp = pcall(require, "cmp")
+		if ok then
+			cmp.event:on("menu_opened", function()
+				vim.b.matchup_matchparen_enabled = false
+			end)
+			cmp.event:on("menu_closed", function()
+				vim.b.matchup_matchparen_enabled = true
+			end)
+		end
+		require("match-up").setup(opts)
 	end,
 }
 
